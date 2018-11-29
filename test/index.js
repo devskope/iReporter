f79998409ed94d58bf703702660f46bf9b8eac22
ope-oguntoye <ope.oguntoye@protonmail.com>
2018-11-29 20:30:03 +0100

    feat(create red-flag) add red-flag creation tests

--- test/index.js
+++ test/index.js
@@ -2,11 +2,13 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import debug from 'debug';
 import server from '../src/server';
-import isAllWell from './isallwellintestland';
+import records from './records';
 
 chai.use(chaiHttp);
+
 const { expect } = chai;
 const logger = debug('iReporter::test:');
-const testArgs = { server, chai, expect, logger };
+const ROOT_URL = `/api/v1`;
+const testArgs = { server, chai, expect, ROOT_URL, logger };
 
-isAllWell(testArgs);
+records(testArgs);
--- test/isallwellintestland.js
+++
@@ -1,7 +0,0 @@
-export default ({ expect, logger }) => {
-  logger('testing to see if setup worked...... (setup? set-up??)');
-
-  it('expect two instances of the exact same string to be equal', async () => {
-    expect('hello, i am iReporter').eq('hello, i am iReporter');
-  });
-};
