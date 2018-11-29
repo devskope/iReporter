469d5436624b55f576307b1f4dcd6ff2d3f4e4a0
ope-oguntoye <ope.oguntoye@protonmail.com>
2018-11-29 20:39:04 +0100

    feat(create red-flag) Client can create new red-flag record [delivers #162289641]

---
+++ src/routes/records/index.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import redFlagController from './redFlagController';
+import strictRecordType from '../../middlewares/records/strictRecordType';
+import {
+  checkRequired,
+  verifyRequestTypes,
+} from '../../middlewares/sanitizeRequest';
+
+const router = new Router();
+
+router.post(
+  '/red-flags',
+  checkRequired('record'),
+  verifyRequestTypes('record'),
+  strictRecordType('red-flag'),
+  redFlagController.createRecord
+);
+
+export default router;
