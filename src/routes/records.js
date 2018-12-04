924eb3b0dd6802469b385ffe597d37fad6d125c3
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-04 09:14:46 +0100

    chore(refactor): extract controllers to higher level in project tree

--- src/routes/records.js
+++ src/routes/records.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import redFlagController from '../controllers/redFlagController';
-import strictRecordType from '../middlewares/records/strictRecordType';
 import {
   checkRequired,
+  strictRecordType,
   verifyRequestTypes,
 } from '../middlewares/sanitizeRequest';
-import loadOneByID from '../middlewares/records/loadOneByID';
+import loadRecordByID from '../middlewares/loadRecordByID';
 
 const router = new Router();
 
@@ -13,32 +13,28 @@ router.post(
   '/red-flags',
   checkRequired('record'),
   strictRecordType('red-flag'),
-  verifyRequestTypes('record'),
+  verifyRequestTypes,
   redFlagController.createRecord
 );
 router.get('/red-flags', redFlagController.fetchAllRecords);
-router.get(
-  '/red-flags/:id',
-  loadOneByID({ what: 'red-flag' }),
-  redFlagController.fetchRecordByID
-);
+router.get('/red-flags/:id', loadRecordByID, redFlagController.fetchRecordByID);
 router.patch(
   '/red-flags/:id/comment',
   checkRequired('comment'),
-  loadOneByID({ what: 'red-flag' }),
-  verifyRequestTypes('record'),
+  loadRecordByID,
+  verifyRequestTypes,
   redFlagController.updateComment
 );
 router.patch(
   '/red-flags/:id/location',
   checkRequired('location'),
-  loadOneByID({ what: 'red-flag' }),
-  verifyRequestTypes('record'),
+  loadRecordByID,
+  verifyRequestTypes,
   redFlagController.updateLocation
 );
 router.delete(
   '/red-flags/:id',
-  loadOneByID({ what: 'red-flag' }),
+  loadRecordByID,
   redFlagController.deleteRecordByID
 );
 
