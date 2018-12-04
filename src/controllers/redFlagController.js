f21dd1a6009f870a3dcbcfebfcbe765deba15f04
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-04 04:14:41 +0100

    chore(Refactor codebase): Extract red-flag controller into to  top level directory

---
+++ src/controllers/redFlagController.js
@@ -0,0 +1,63 @@
+import { RedFlag } from '../models/records';
+import successResponse from '../helpers/successResponse';
+
+const createRecord = (req, res) => {
+  const { title, comment, location } = req.body;
+
+  const newRedFlag = new RedFlag({ title, comment, location });
+  newRedFlag
+    .save()
+    .then(({ id }) =>
+      successResponse(res, { id, message: `Created red-flag record` }, 201)
+    );
+};
+
+const fetchAllRecords = (req, res) => {
+  RedFlag.getAll().then(redFlagRecords =>
+    redFlagRecords.length > 0
+      ? successResponse(res, redFlagRecords)
+      : successResponse(res)
+  );
+};
+
+const fetchRecordByID = ({ record }, res) => successResponse(res, record);
+
+const updateComment = ({ body, record }, res) =>
+  record.comment !== body.comment
+    ? RedFlag.patch(record.id, body.comment, 'comment').then(({ id }) =>
+        successResponse(res, {
+          id,
+          message: 'Updated red-flag record’s comment',
+        })
+      )
+    : successResponse(res, {
+        id: record.id,
+        message: 'Comment unchanged, nothing to update',
+      });
+
+const updateLocation = ({ body, record }, res) =>
+  record.location !== body.location
+    ? RedFlag.patch(record.id, body.location, 'location').then(({ id }) =>
+        successResponse(res, {
+          id,
+          message: 'Updated red-flag record’s location',
+        })
+      )
+    : successResponse(res, {
+        id: record.id,
+        message: 'Location unchanged, nothing to update',
+      });
+
+const deleteRecordByID = ({ record }, res) =>
+  RedFlag.delete(record.id).then(({ id }) =>
+    successResponse(res, { id, message: 'red-flag record has been deleted' })
+  );
+
+export default {
+  createRecord,
+  fetchAllRecords,
+  fetchRecordByID,
+  updateComment,
+  updateLocation,
+  deleteRecordByID,
+};
---
+++ src/routes/records.js
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+import redFlagController from '../controllers/redFlagController';
+import strictRecordType from '../middlewares/records/strictRecordType';
+import {
+  checkRequired,
+  verifyRequestTypes,
+} from '../middlewares/sanitizeRequest';
+import loadOneByID from '../middlewares/records/loadOneByID';
+
+const router = new Router();
+
+router.post(
+  '/red-flags',
+  checkRequired('record'),
+  strictRecordType('red-flag'),
+  verifyRequestTypes('record'),
+  redFlagController.createRecord
+);
+router.get('/red-flags', redFlagController.fetchAllRecords);
+router.get(
+  '/red-flags/:id',
+  loadOneByID({ what: 'red-flag' }),
+  redFlagController.fetchRecordByID
+);
+router.patch(
+  '/red-flags/:id/comment',
+  checkRequired('comment'),
+  loadOneByID({ what: 'red-flag' }),
+  verifyRequestTypes('record'),
+  redFlagController.updateComment
+);
+router.patch(
+  '/red-flags/:id/location',
+  checkRequired('location'),
+  loadOneByID({ what: 'red-flag' }),
+  verifyRequestTypes('record'),
+  redFlagController.updateLocation
+);
+router.delete(
+  '/red-flags/:id',
+  loadOneByID({ what: 'red-flag' }),
+  redFlagController.deleteRecordByID
+);
+
+export default router;
