5c92b0ac93dff3d58de5320101ca4d8226b84ac9
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-09 10:40:14 +0100

    bug(fix-models): refactor admin save [Finishes #162529795]

--- src/models/users.js
+++ src/models/users.js
@@ -20,34 +20,9 @@ export class User {
     this.isAdmin = false;
   }
 
-  save(mode) {
-    let queryString;
-
-    if (mode.admin)
-      queryString = [
-        `INSERT INTO records(firstname,
-          lastname,
-          othernames,
-          username,
-          password,
-          email,
-          phone, is_admin)
-         VALUES($1, $2, $3, $4, $5, $6, $7, $8)
-         RETURNING *`,
-        [
-          this.firstname,
-          this.lastname,
-          this.othernames,
-          this.username,
-          this.password,
-          this.email,
-          this.phone,
-          this.isAdmin,
-        ],
-      ];
-    else
-      queryString = [
-        `INSERT INTO users(firstname,
+  save() {
+    const queryString = [
+      `INSERT INTO users(firstname,
           lastname,
           othernames,
           username,
@@ -56,16 +31,16 @@ export class User {
           phone)
          VALUES($1, $2, $3, $4, $5, $6, $7)
          RETURNING *`,
-        [
-          this.firstname,
-          this.lastname,
-          this.othernames,
-          this.username,
-          this.password,
-          this.email,
-          this.phone,
-        ],
-      ];
+      [
+        this.firstname,
+        this.lastname,
+        this.othernames,
+        this.username,
+        this.password,
+        this.email,
+        this.phone,
+      ],
+    ];
     return db.query(...queryString);
   }
 }
@@ -74,5 +49,30 @@ export class Admin extends User {
   constructor({ ...args }) {
     super(args);
     this.isAdmin = true;
+  }
+
+  save() {
+    const queryString = [
+      `INSERT INTO records(firstname,
+        lastname,
+        othernames,
+        username,
+        password,
+        email,
+        phone, is_admin)
+       VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+       RETURNING *`,
+      [
+        this.firstname,
+        this.lastname,
+        this.othernames,
+        this.username,
+        this.password,
+        this.email,
+        this.phone,
+        this.isAdmin,
+      ],
+    ];
+    return db.query(...queryString);
   }
 }
