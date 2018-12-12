589b05d3cf1dc2db0ef98c780bfe6928d2c54754
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-12 08:57:10 +0100

    feat(statusMutations): provision default admin user

--- src/config/envConf.js
+++ src/config/envConf.js
@@ -2,10 +2,28 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const { env } = process;
+const {
+  ADMIN_EMAIL,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
+  ADMIN_FIRST_NAME,
+  ADMIN_LAST_NAME,
+  NODE_ENV,
+  PORT,
+  DATABASE_URL,
+  JWT_SECRET,
+} = env;
 
 export default {
-  NODE_ENV: env.NODE_ENV,
-  PORT: env.PORT,
-  DATABASE_URL: env.DATABASE_URL,
-  JWT_SECRET: env.JWT_SECRET,
+  ADMIN: {
+    email: ADMIN_EMAIL,
+    username: ADMIN_USERNAME,
+    password: ADMIN_PASSWORD,
+    firstname: ADMIN_FIRST_NAME,
+    lastname: ADMIN_LAST_NAME,
+  },
+  DATABASE_URL,
+  JWT_SECRET,
+  NODE_ENV,
+  PORT,
 };
