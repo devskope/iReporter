f622333715193600fbb22376a4adff31c18c2e0c
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-13 23:56:26 +0100

    bug(db): monkey patch possible int overflow

--- src/middlewares/loadRecordByID.js
+++ src/middlewares/loadRecordByID.js
@@ -4,20 +4,21 @@ import handleError from '../helpers/errorHelper';
 
 export default type => (req, res, next) => {
   const { id } = req.params;
-
-  Record.getOneByID(jsonParse(id)).then(({ rows }) =>
-    rows[0] && rows[0].type === type
-      ? (() => {
-          if (
-            !['under investigation', 'resolved', 'rejeted'].includes(
-              rows[0].status
-            )
-          ) {
-            req.editable = true;
-          }
-          req.record = { ...rows[0] };
-          next();
-        })()
-      : handleError(res, `No ${type} record exists with id '${id}'`, 404)
-  );
+  return id > 999999999
+    ? handleError(res, `No record exists with id '${id}'`, 404)
+    : Record.getOneByID(jsonParse(id)).then(({ rows }) =>
+        rows[0] && rows[0].type === type
+          ? (() => {
+              if (
+                !['under investigation', 'resolved', 'rejeted'].includes(
+                  rows[0].status
+                )
+              ) {
+                req.editable = true;
+              }
+              req.record = { ...rows[0] };
+              next();
+            })()
+          : handleError(res, `No ${type} record exists with id '${id}'`, 404)
+      );
 };
