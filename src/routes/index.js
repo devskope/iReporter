723b170e4190ad6e5781b03b63469848c2571d82
ope oguntoye <ope.oguntoye@protonmail.com>
2018-12-17 12:55:26 +0100

    chore(documentation): serve swagger documentation  [delivers #162627982]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import recordRouter from './records';
 import authRouter from './auth';
+import apiSpec from '../../utils/swagger_spec.json';
+
+const { serve, setup } = require('swagger-ui-express');
 
 const ROOT_URL = '/api/v1';
 
 const router = app => {
   app.use(ROOT_URL, recordRouter);
   app.use(ROOT_URL, authRouter);
+  app.use('/api-docs', serve, setup(apiSpec));
 };
 
 export default router;
