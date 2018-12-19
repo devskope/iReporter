bc52182113f17a1ba70e9176d5809f1a1e4ce4ac
ope-oguntoye <ope.oguntoye@protonmail.com>
2018-12-19 04:34:10 +0100

    feat(email-notifications): Add tests for email notifications

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ import server from '../src/server';
 import records from './records';
 import users from './users';
 import tokenAuth from './tokenAuth';
+import mailer from './mailer';
 
 chai.use(chaiHttp);
 
@@ -16,3 +17,4 @@ const testArgs = { server, chai, expect, ROOT_URL, logger };
 tokenAuth(testArgs);
 records(testArgs);
 users(testArgs);
+mailer(testArgs);
