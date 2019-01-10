3654b935e135987b2971273343b7663dde0ee312
ope oguntoye <ope.oguntoye@protonmail.com>
2019-01-10 11:10:34 +0100

    Implment UI responsiveness
    - Add more semantic CSS class names to template markups
    - Implement responsiveness on all UI templates
    [delivers #163110577]

--- UI/assets/js/dash.js
+++ UI/assets/js/dash.js
@@ -13,6 +13,22 @@ const coordDisplay = document.querySelector('.create-edit-form__geocodes');
 const locationReset = document.querySelector(
   '.create-edit-form__location-reset'
 );
+const statusFilterContainer = document.querySelector(
+  '.dashboard__filter-categories'
+);
+const mobileFilterToggle = document.querySelector('.dashboard__filter-toggle');
+
+const toggleMobileFilter = () => {
+  statusFilterContainer.classList.toggle('active');
+  mobileFilterToggle.classList.toggle('active');
+};
+
+const fieldsBelow = document.querySelectorAll(
+  '.form-field--media, .btn--submit'
+);
+
+if (mobileFilterToggle)
+  mobileFilterToggle.addEventListener('click', toggleMobileFilter);
 
 /* Bind listeners to all modal toggles */
 [...openModal, ...closeModal].forEach(element =>
@@ -40,26 +56,38 @@ btnsEditRecord.forEach(element =>
 const getCoords = ({ lng, lat }) => `${lng()},${lat()}`;
 
 const displayCoords = coordinateString => {
+  coordDisplay.hidden = false;
   coordDisplay.textContent = coordinateString;
+
+  fieldsBelow.forEach(field => field.classList.add('pushdown'));
+
   coordDisplay.classList.remove('hidden');
 };
 
-const liveUpdate = new window.google.maps.places.Autocomplete(locationInput);
-liveUpdate.addListener('place_changed', () => {
-  const geoLocation = liveUpdate.getPlace();
-  if (geoLocation.geometry) {
-    const coordinateString = getCoords(geoLocation.geometry.location);
-    displayCoords(coordinateString);
-  } else {
-    alert(
-      `Error: no geospatial coordinates availabe for "${geoLocation.name}"`
-    );
-  }
-});
+if (window.google && locationInput) {
+  const liveUpdate = new window.google.maps.places.Autocomplete(locationInput);
+  liveUpdate.addListener('place_changed', () => {
+    const geoLocation = liveUpdate.getPlace();
+    if (geoLocation.geometry) {
+      const coordinateString = getCoords(geoLocation.geometry.location);
+      displayCoords(coordinateString);
+    } else {
+      alert(
+        `Error: no geospatial coordinates availabe for "${geoLocation.name}"`
+      );
+    }
+  });
+}
 
 const resetLocationFields = () => {
-  locationInput.value = '';
-  coordDisplay.value = '';
   coordDisplay.hidden = true;
+  coordDisplay.classList.add('hidden');
+
+  fieldsBelow.forEach(field => field.classList.remove('pushdown'));
+
+  coordDisplay.value = '';
+  locationInput.value = '';
 };
-locationReset.addEventListener('click', () => resetLocationFields());
+
+if (locationReset)
+  locationReset.addEventListener('click', () => resetLocationFields());
