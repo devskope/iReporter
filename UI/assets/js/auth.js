b87bb3b09e57225b506704df7139a72c826dc119
ope oguntoye <ope.oguntoye@protonmail.com>
2019-01-11 22:17:57 +0100

    Implement frontend signup functionality
    - Enabled CORS on API server.
    - Extended type validations for incoming request params.
    - Created global IR_HELPERS object in frontend to hold shared code
    - Consume API signup endpoint on frontend (Implement account creation functionality)
    
    [delivers #163141848]

--- UI/assets/js/auth.js
+++ UI/assets/js/auth.js
@@ -1,11 +1,35 @@
-const loginForm = document.querySelector('.auth__form--login');
+this.addEventListener('load', () => {
+  const signupForm = document.querySelector('.auth__form--signup');
 
-loginForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const { currentTarget: form } = e;
-  const username = form.querySelector('.form-field__login-username');
-  const password = form.querySelector('.form-field__login-password');
+  const {
+    findMissingFields,
+    makeAuthFormMessages,
+    missingFieldsMessage,
+    signUp,
+    responsiveNav,
+  } = window.IR_HELPERS;
 
-  if (username.value === 'admin' && password.value === 'admin')
-    window.location.assign('admin.html');
+  responsiveNav();
+
+  if (signupForm) {
+    signupForm.addEventListener('submit', e => {
+      e.preventDefault();
+
+      const { currentTarget: form } = e;
+      const { value: firstname } = form.querySelector('.form-field__firstname');
+      const { value: lastname } = form.querySelector('.form-field__lastname');
+      const { value: username } = form.querySelector('.form-field__username');
+      const { value: email } = form.querySelector('.form-field__email');
+      const { value: password } = form.querySelector('.form-field__password');
+
+      const missingFields = findMissingFields({ username, email, password });
+
+      return missingFields.length > 0
+        ? makeAuthFormMessages({
+            type: 'error',
+            messages: [missingFieldsMessage(missingFields)],
+          })
+        : signUp(firstname, lastname, username, email, password);
+    });
+  }
 });
