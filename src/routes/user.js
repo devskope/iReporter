bb658e6d0813fb8bd336039a34a028c8bfc0b1f2
ope oguntoye <ope.oguntoye@protonmail.com>
2019-01-11 23:13:08 +0100

    chore(update gh-pages): Update frontend pages
    - Update gh-pages with latest changes from develop branch
    - Remove non UI files from gh-pages
    
    [delivers #163141749]

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import userController from '../controllers/userController';
-import validUser from '../middlewares/validUser';
-
-const router = new Router();
-
-router.get('/user/recordstats', validUser, userController.fetchStats);
-
-export default router;
