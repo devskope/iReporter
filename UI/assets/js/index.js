e521ae7baa963787f02ee8b978cc3f53708d867f
ope-oguntoye <ope.oguntoye@protonmail.com>
2019-01-12 01:59:02 +0100

    chore(refactor): Prune  dev branch
    - Removed UI templates from the develop branch : these live in the gh-pages branch  and updates  should be applied to branches forked from gh-pages
    
    [delivers #163164122]

--- UI/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const accessBtns = document.querySelectorAll('.btn.access');
-
-accessBtns.forEach(btn =>
-  btn.addEventListener('click', () =>
-    window.location.assign('./UI/views/signup.html')
-  )
-);
