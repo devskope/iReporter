54c641799e8ed71a82258e61b204420749130981
ope-oguntoye <ope.oguntoye@protonmail.com>
2019-01-18 11:21:05 +0100

    ch(update-branch): Update gh-pages with recent changes

--- src/helpers/jsonParse.js
+++
@@ -1,9 +0,0 @@
-export default arg => {
-  let parsed;
-  try {
-    parsed = JSON.parse(arg);
-  } catch (e) {
-    return null;
-  }
-  return parsed;
-};
--- src/helpers/successResponse.js
+++
@@ -1,4 +0,0 @@
-export default (response, data = [], code = 200) =>
-  response
-    .status(code)
-    .json({ status: code, data: data instanceof Array ? data : [data] });
