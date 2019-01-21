d730bc4592c12251d3ea0c823768a70bd6484f84
ope-oguntoye <ope.oguntoye@protonmail.com>
2019-01-21 08:09:57 +0100

    ch(ui): tweak ui appearance
    - Hide mobile menu when user stops interacting with it.
    - Improve alignment of elements within individual dashboard feed components
    - Tweak colors and dimensions
    
    [finishes #163355071]

--- UI/assets/js/index.js
+++ UI/assets/js/index.js
@@ -1,7 +1,28 @@
+const { getToken, responsiveNav } = window.IR_HELPERS;
+
 const accessBtns = document.querySelectorAll('.btn.access');
 
-accessBtns.forEach(btn =>
-  btn.addEventListener('click', () =>
-    window.location.assign('./UI/views/signup.html')
-  )
-);
+responsiveNav();
+
+if (!getToken()) {
+  accessBtns.forEach(btn =>
+    btn.addEventListener('click', () =>
+      window.location.assign('./UI/views/signup.html')
+    )
+  );
+} else {
+  const navLoginBtn = document.querySelector('.btn.btn--nav');
+  const footerRegLink = document.querySelector('.footer__link-join');
+
+  navLoginBtn.href = './UI/views/user.html';
+  navLoginBtn.textContent = 'Dashboard';
+
+  accessBtns.forEach(btn => {
+    btn.addEventListener('click', () =>
+      window.location.assign('./UI/views/user.html')
+    );
+    btn.textContent = 'Latest Reports';
+  });
+
+  footerRegLink.style.display = 'none';
+}
